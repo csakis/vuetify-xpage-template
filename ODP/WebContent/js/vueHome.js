42dada23a51611889da393ad0bc8ef251be87446
Csaba Kiss <csakis@lanl.gov>
2018-02-21 14:04:45 -0700

    added tabs and and tab contents

--- ODP/WebContent/js/vueHome.js
+++ ODP/WebContent/js/vueHome.js
@@ -2,10 +2,12 @@ new Vue({
 	el: '#home',
 	data: {
 		isDarkTheme: false,
-		themeList: ["red", "green", "blue", "yellow"]
-		
+		themeList: ["red", "green", "blue", "yellow"],
+		tabList: ["Event", "Exercise", "Test"],
+		tab: null
+
 	},
 	props: {
-		
+
 	}
 });
